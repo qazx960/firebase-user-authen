8677ea5f51e039a8ae53a58ea807b0879a1916da
Jiwon Lee <giwon.yi339@gmail.com>
2023-05-30 22:45:41 +0900

    edited

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Signup from "./auth/Signup";
 
 export default function ErrorPage() {
   return (
     <div>
-      <h1>Page not found Error 404</h1>
-      <Link to="/">Signup</Link>
-      <Link to="/login">Login</Link>
+      <h1>Signup or Login</h1>
+      <div id="homepage">
+        <Link to="/">Signup</Link>
+        <Link to="/login">Login</Link>
+      </div>
     </div>
   );
 }
